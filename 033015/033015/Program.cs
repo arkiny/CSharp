8935aef008679c42c62918ec9ec49a5be26162e3
Heedong Lee <arkinylee@gmail.com>
2015-03-30 12:16:32 +0900

    0330 csharp start

---
+++ 033015/033015/Program.cs
@@ -0,0 +1,27 @@
+﻿// Namespace should be declared in the class
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _033015
+{
+    class MyProgram
+    {
+        // Entry Point should be exist only one.
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello Shitty BullShit World");
+        }
+    }
+    class Program
+    {
+        // Two Entry Point will make compile Error
+        //static void Main(string[] args)
+        //{
+        //    Console.WriteLine("Hello Shitty BullShit World");
+        //}
+    }
+}
